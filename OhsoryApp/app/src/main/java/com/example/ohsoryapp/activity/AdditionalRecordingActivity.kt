457c2eb1acf1a0647553b78b405200418f8d195e
weenybeenymini <alsrjs2516@naver.com>
2020-04-18 22:24:01 +0900

    before_progress_bar

---
+++ OhsoryApp/app/src/main/java/com/example/ohsoryapp/activity/AdditionalRecordingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ohsoryapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ohsoryapp.R
+
+class AdditionalRecordingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_additional_recording)
+    }
+}
---
+++ OhsoryApp/app/src/main/java/com/example/ohsoryapp/activity/ModelManageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ohsoryapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ohsoryapp.R
+
+class ModelManageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_model_manage)
+    }
+}
